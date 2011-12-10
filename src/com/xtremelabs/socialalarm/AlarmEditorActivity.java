afebeb29627d2860b534b1a5791935d65a261d88
Charles Cheung and Tom McNeely <charles.cheung@xtremelabs.com>
2011-12-10 17:39:08 -0500

    Tom - Worked on the alarm editor activity.

--- src/com/xtremelabs/socialalarm/AlarmEditorActivity.java
+++ src/com/xtremelabs/socialalarm/AlarmEditorActivity.java
@@ -1,13 +1,41 @@
 package com.xtremelabs.socialalarm;
 
+import java.util.Calendar;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.xtremelabs.socialalarm.util.RingUtil;
 
 public class AlarmEditorActivity extends Activity {
     
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.alarm_editor);
+        
+//        findViewById(R.id.time_picker)
+        
+//        findViewById(R.id.alarm_name)
+        
+        findViewById(R.id.choose_ringtone).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				RingUtil.pickRing(AlarmEditorActivity.this);
+			}
+		});
+        
+        findViewById(R.id.done_button).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
     }
 }
