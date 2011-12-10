9ebf150576c24dd81a6042bbcdf3db38fba54b71
Hannah Mittelstaedt & Rebecca Putinski <sabesan.shanmugabhavananthan@xtremelabs.com>
2011-12-10 12:39:30 -0500

    HM - put in FB sdk

--- src/com/xtremelabs/socialalarm/LaunchActivity.java
+++ src/com/xtremelabs/socialalarm/LaunchActivity.java
@@ -3,6 +3,8 @@ package com.xtremelabs.socialalarm;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.xtremelabs.socialalarm.R;
+
 public class LaunchActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
