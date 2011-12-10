ccd9a656899393b64d8855a74b1c6e1036236caa
Charles Cheung and Tom McNeely <charles.cheung@xtremelabs.com>
2011-12-10 16:06:13 -0500

    Tom - Created alarm list activity and alarm editor activity.

---
+++ src/com/xtremelabs/socialalarm/SocialAlarmApplication.java
@@ -0,0 +1,13 @@
+package com.xtremelabs.socialalarm;
+
+import android.app.Application;
+
+public class SocialAlarmApplication extends Application {
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		
+		
+	}
+}
---
+++ src/com/xtremelabs/socialalarm/util/TimeFormatter.java
@@ -0,0 +1,12 @@
+package com.xtremelabs.socialalarm.util;
+
+import android.text.format.Time;
+
+public class TimeFormatter {
+
+	private static final String TIME_FORMAT = "%l:%M";
+	
+	public static String formatTime(Time time) {
+		return time.format(TIME_FORMAT);
+	}
+}
