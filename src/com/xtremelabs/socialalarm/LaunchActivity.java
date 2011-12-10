a1f92d10b459b8f42e58232af0758f7dd6e9d2d6
Tony W and Hannah M <tony@xtremelabs.com>
2011-12-10 11:52:45 -0500

    TW/HM: project setup

---
+++ src/com/xtremelabs/socialalarm/LaunchActivity.java
@@ -0,0 +1,13 @@
+package com.xtremelabs.socialalarm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LaunchActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
